4b65951486df3753f1c31e424f06967128e07537
Jepoy <poyhidalgo@gmail.com>
2026-02-04 17:35:29 +0800

    chore: add EmailNotification model and update ForeignKey references to use settings.AUTH_USER_MODEL

--- backend/apps/notifications/models.py
+++ backend/apps/notifications/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 class EmailNotification(models.Model):
-    user = models.ForeignKey('auth.user', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     ticket = models.ForeignKey('tickets.Ticket', on_delete=models.CASCADE)
     event = models.CharField(max_length=100)
     sent_at = models.DateTimeField(auto_now_add=True)
