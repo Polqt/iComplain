44d604f700c1ca8b5f9f96ac49750cd299f140ce
Maulvi Jinn Limpao <s1910528@usls.edu.ph>
2026-02-04 16:11:43 +0800

    chore: remove unnecessary blank line in models.py

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -2,7 +2,6 @@ from django.db import models
 from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 
-
 # Create your models here.
 class CustomUserManager(BaseUserManager): #custom user model since intended to use email and password only
     def create_user(self, email, password=None, **extra_fields):
